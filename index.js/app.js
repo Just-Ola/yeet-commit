0cff2212b37c642ed00c9eaa29971bcb81e4d669
Just Ola <owohamzat@gmail.com>
2025-11-19 09:47:58 +0100

    first commit

--- index.js/app.js
+++ index.js/app.js
@@ -4,3 +4,13 @@ console.log(age)
 
 const address= 59;
 console.log(address)
+
+let num1= 45;
+let num2= 23;
+let sum= num1 + num2;
+console.log(sum)    
+
+let num3= 45;
+let num4= 23;
+let product= num1 * num2;
+console.log(product)
---
+++ index.js/condition.js
@@ -0,0 +1,33 @@
+var votingAge = 18;
+
+var votingAge = 15;
+
+if (votingAge >= 18) {
+    console.log("You can vote")
+}
+else{
+    console.log("You cannot vote")
+}
+
+
+var myChoice="Rice"
+var available="Spag"
+
+if(myChoice==available) {
+    console.log("You can buy")
+}  else{
+    console.log("Don't buy")
+}
+
+
+var age= 16
+paidTution= "True"
+passedJAMB="True"
+Have5credits="True"
+
+if( age>= 16 && paidTution && passedJAMB && Have5credits){
+    console.log("You r admitted")
+}else{
+    console.log ("You are not admitted")
+}
+
