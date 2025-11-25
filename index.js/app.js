668ac939b004064d2bf4a1e971d40cc730e4c376
Just Ola <owohamzat@gmail.com>
2025-11-25 10:43:25 +0100

    Edited commit

--- index.js/app.js
+++ index.js/app.js
@@ -1,3 +1,5 @@
+//Variable Declarations and Basic Operations
+
 let age= 78;
 age= 17;
 console.log(age)
@@ -14,3 +16,63 @@ let num3= 45;
 let num4= 23;
 let product= num1 * num2;
 console.log(product)
+
+
+//String Methods and talking about characters
+let text1= "Hello ";
+
+let char= text1.charAt(4);
+
+console.log(char)
+
+
+const fulName= "Vanessa Jennifer";
+let chaar = fulName.charAt(6);
+let charr = fulName.charAt(8);
+console.log(chaar);
+console.log(charr);
+
+
+let firstName= "Vanessa";   
+let lastName= "Jennifer";
+
+let fullName= firstName + " " + lastName;
+console.log(fullName);
+
+
+
+function fullName1(firstName1, lastName1){
+    return firstName1 + " " + lastName1;
+}
+console.log(fullName1("Munachi", "Mazi"));
+
+
+
+
+//Length, breadth and area of a rectangle
+let length= 7;
+let breadth= 8;
+let area= length * breadth;
+console.log(area);
+
+function area1(length1, breadth1){
+    return length1 * breadth1;
+}
+console.log(area1(3,7));
+
+
+
+function area2 (length2, length2){
+    return length2 * length2;
+}
+console.log(area2(5,5));
+
+
+
+///Arrow Functions:
+let myFunction= (a, b) => a * b;
+console.log(myFunction(4, 3));
+
+let myName= (c, d) => c + "" + d;
+
+    console.log(myName("Ami", "Ola"));
